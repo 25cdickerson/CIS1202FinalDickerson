27a2bec6b8ec3e8ef2d7c34b20c0ccda79ab1d4e
Christopher Dickerson <christopher.dickers1@sinclair.edu>
2020-12-11 14:14:31 -0500

    Setters and Getters added onto the header and implementation files.

--- CIS1202Final/Trip.cpp
+++ CIS1202Final/Trip.cpp
@@ -10,6 +10,17 @@
 
 using namespace std;
 
+Trip::Trip() {
+	name = "null";
+	date = "null";
+	tex = -1;
+	fex = -1;
+	vex = -1;
+	oex = -1;
+	total = -1;
+}
+
+// Constuctor Declaration
 Trip::Trip(string n, string d, int t, int f, int v, int o, int to) {
 	name = n;
 	date = d;
@@ -20,4 +31,61 @@ Trip::Trip(string n, string d, int t, int f, int v, int o, int to) {
 	total = to;
 }
 
+// Getter Declarations
+string Trip::getName() {
+	return name;
+}
 
+string Trip::getDate() {
+	return date;
+}
+
+int Trip::getTex() {
+	return tex;
+}
+
+int Trip::getFex() {
+	return fex;
+}
+
+int Trip::getVex() {
+	return vex;
+}
+
+int Trip::getOex() {
+	return oex;
+}
+
+int Trip::getTotal() {
+	return total;
+}
+
+// Setter Declarations
+
+void Trip::setName(string s) {
+	name = s
+}
+
+void Trip::setDate(string s) {
+	date = s;
+}
+
+void Trip::setTex(int i) {
+	tex = i;
+}
+
+void Trip::setFex(int i) {
+	fex = i;
+}
+
+void Trip::setVex(int i) {
+	vex = i;
+}
+
+void Trip::setOex(int i) {
+	oex = i;
+}
+
+void Trip::setTotal(int i) {
+	total = i;
+}
--- CIS1202Final/Trip.h
+++ CIS1202Final/Trip.h
@@ -24,11 +24,27 @@ private:
 	int total;
 
 public:
+	Trip();
 	Trip(string n, string d, int t, int f, int v, int o, int to);
+	
+	// Getters
+	string getName();
+	string getDate();
+	int getTex();
+	int getFex();
+	int getVex();
+	int getOex();
+	int getTotal();
+
+	// Setters
+	void setName(string s);
+	void setDate(string s);
+	void setTex(int i);
+	void setFex(int i);
+	void setVex(int i);
+	void setOex(int i);
+	void setTotal(int i);
 };
 
-// Getters
-
-// Setters
 
 #endif
